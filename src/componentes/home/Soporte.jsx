31f6b11dbc4cc5fe223a4d66a234c438d7866d58
snow915 <cris_ganor@hotmail.com>
2021-03-08 12:37:11 -0600

    fix: Ventajas and Soporte sections in Home, change img for text

--- src/componentes/home/Soporte.jsx
+++ src/componentes/home/Soporte.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import Wallet from "../../images/wallet.svg";
-import Openzepp from "../../images/openzepp.svg";
-import Github from "../../images/github.svg";
-import Hardwallet from "../../images/hardwallet.svg";
+import Openzepp from "../../images/zeppelin_icon.svg";
+import Github from "../../images/github_icon.svg";
 import 'bootstrap/dist/css/bootstrap.css';
+import "./css/styles.css";
 
 
 const Soporte = () => {
+
+    const items = [
+        {
+            icon: Github,
+            title: "GITHUB",
+            text: "Plataforma web de alojamiento de repositorios de código fuente para el control de versiones"
+        },
+        {
+            icon: Openzepp,
+            title: "OPENZEPPELIN",
+            text: "Permite crear contratos inteligentes de tipo ERC20, a través de una API estable, uno de sus principales objetivos es garantizar la seguridad de los contratos una vez que éstos se encuentren liberados."
+        }
+    ];
+
     return (
         <div className="row">
             <div className="col-12 fondoedi">
                 <p className="text-soporte">
                     SOCIOS Y SOPORTE
                 </p>
-                <div className=" row fondo-opa">
-                <img src={Github} className="img-meto col-12 col-lg-4  col-md-4  "/>
-                    <img src={Openzepp} className="img-meto col-lg-4  col-md-4 col-12"/>
+                <div className="row fondo-opa d-flex justify-content-center pb-5">
+                {
+                    items.map((value, index) => (
+                            <div key={index} className="img-meto m-4 col-12 col-lg-4 col-md-4">
+                                <img src={value.icon} className="img-fluid w-25 mt-1 mt-lg-5" alt=""/>
+                                <h4 className="mt-3">{value.title}</h4>
+                                <p className="mt-3 pl-xl-5 pr-xl-5 lead">
+                                    {value.text}
+                                </p>
+                            </div>
+
+                    ))
+                }
                 </div>
-
-
-               
-
             </div>
         </div>
     );
--- src/componentes/home/Ventajas.jsx
+++ src/componentes/home/Ventajas.jsx
@@ -5,23 +5,47 @@ import seguro from "../../images/seguro.png";
 import Seguromovil from "../../images/seguromovil.svg";
 import transp from "../../images/transparante.png";
 import Transmovil from "../../images/transmovil.svg";
+import Candado from "../../images/candado.svg";
+import Mano from "../../images/mano_icon.svg";
+import Escudo from "../../images/escudo.svg";
 
 const Ventajas = () => {
-    return (
-        <div className="  row ventajascont ">
-            <div className="col-md-4 col-12 ventajas  p-0">
-                <img src={facil} alt="FACIL" className="d-none d-md-block img-vent"/>
-                <img src={Facilmovil} alt="FACIL" className="d-block d-md-none img-vent"/>
-            </div>
-            <div className="col-md-4   col-12 ventajas d-flex flex-row-reverse ventajas-seg p-0">
-                <img src={seguro} alt="segurp" className="d-none d-md-block img-vent"/>
-                <img src={Seguromovil} alt="FACIL" className="d-block d-md-none img-vent"/>
-            </div>
-            <div className="col-md-4 col-12 p-0   ventajas">
-                <img src={transp} alt="transp.png" className="d-none d-md-block img-vent"/>
-                <img src={Transmovil} alt="FACIL" className="d-block d-md-none img-vent"/>
 
-            </div>
+    const ventajas = [
+        {
+            icon: Mano,
+            title: "DE USO FÁCIL",
+            description: "Diseño accesible para todos los Sunholders, creado para que sea práctico, cómodoy rápido poder adquirir el SUN"
+        },
+        {
+            icon: Candado,
+            title: "SEGURO",
+            description: "Procesamiento confiable, el SUN está asentado sobre el protocolo de Blockchain Ethereum, lo que lo hace privado y a prueba de cualquier hackeo"
+        },
+        {
+            icon: Escudo,
+            title: "TRANSPARENTE",
+            description: "En Sunshine estamos comprometidos a informar, explicar y justificar cada proyecto financiado con el SUN, aclarar las dudas de nuestros Sunholders y ser siempre claros y precisos brindando toda la información correspondiente a las inversiones"
+        },
+    ];
+
+    return (
+        <div className="row">
+            {
+                ventajas.map((value, index) => (
+                    <div key={index} className="col-md-4 col-12 p-5">
+                        <div className="col-12 d-flex justify-content-center mt-5">
+                            <img src={value.icon} alt="FACIL" className="img-fluid"/>
+                        </div>
+                        <div className="col-12 d-flex justify-content-center mt-5 text-justify">
+                            <h3>{value.title}</h3>
+                        </div>
+                        <div className="col-12 d-flex justify-content-center mt-2 text-justify">
+                            <h5>{value.description}</h5>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 };
