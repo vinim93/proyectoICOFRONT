840e6092ab00c7f8e0345b24db5441ee943c19c4
snow915 <snow915@protonmail.com>
2021-03-12 11:39:58 -0600

    add: buyToken function that returns an error or an paymentMethod succesfuly

--- src/componentes/user/Dashboard.jsx
+++ src/componentes/user/Dashboard.jsx
@@ -57,7 +57,7 @@ const Dashboard = () => {
                     </div>
 
                     <DollarMarktComponent />
-                    <PaymentComponent />
+                    <PaymentComponent coinImage={moneda_dashboard} />
 
                 </div>
             )
--- src/componentes/user/PaymentComponent.jsx
+++ src/componentes/user/PaymentComponent.jsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import {Elements, CardElement} from '@stripe/react-stripe-js';
+import finalCoin from '../../images/monedafinal.png';
+import './css/paymentComponent.css';
+import React, {useState} from 'react';
+import {Elements, CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 const stripePromise = loadStripe('pk_test_51IUDGUD9LA3P3AmKfFAk32py2vEcZs0LEw7FWhU8Ebp1YgNqJK09LkJyo11b5dCXWk6ZluCo3JBmTTdbSTc61EKq00EqsKyM49');
 
 const CheckoutForm = () => {
+
+    const stripe = useStripe();
+    const elements = useElements();
+    const [currency, setCurrency] = useState(null);
+
+    const buyToken = async(e) => {
+        e.preventDefault();
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement)
+        })
+
+        if(!error){
+            console.log(paymentMethod);
+        }
+    }
+
     return (
-        <form action="">
-            <CardElement />
+        <form onSubmit={buyToken}>
+            <div className="container">
+                <div className="row m-3">
+                    <div className="col-12">
+                        <div className="pl-5 pr-5">
+                            <div className="input-group pl-5 pr-5">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-text">$</div>
+                                </div>
+                                <input type="text"
+                                       className="form-control"
+                                       id="inlineFormInputGroupUsername"
+                                       placeholder="Cantidad en dolares"
+                                       value={currency}
+                                       onChange={e => setCurrency(e.target.value)}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-12">
+                        <CardElement />
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-12">
+                        <button className="btn btn-lg btn-primary">PAGAR</button>
+                    </div>
+                </div>
+            </div>  
         </form>
     )
 }
 
-const PaymentSection = () => {
+const PaymentSection = ({coinImage}) => {
     return (
-        <Elements stripe={stripePromise}>
-            <CheckoutForm />
-        </Elements>
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <img src={finalCoin} style={{width: 150}} className="img-fluid" alt="SUNSHINE COIN IMAGE"/>
+                        <h5 className="currency-value-title font-weight-bold mt-3">1 USD = 1 SUN</h5>
+                    </div>
+                </div>
+            </div>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
+        </div>
+
     )
 }
 
-const PaymentComponent = () => {
+const PaymentComponent = ({coinImage}) => {
     return (
         <div className="modal fade" id="paymentModal" tabIndex="-1" role="dialog"
              aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -33,7 +91,7 @@ const PaymentComponent = () => {
                     </div>
                     <div className="modal-body">
 
-                        <PaymentSection />
+                        <PaymentSection coinImage={coinImage} />
 
                     </div>
                 </div>
