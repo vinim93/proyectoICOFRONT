7873a002e69c95ca5713edabcb85a59fda6922ad
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-03-17 10:20:21 -0600

    se arregla banner2

--- src/componentes/home/Video.jsx
+++ src/componentes/home/Video.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Video = () => {
     return (
         <div className="row   ">
-            <div className="col-12 videosunrep ">
+            <div className="col-12 videosunrep text-center ">
                 <iframe src="https://www.youtube.com/embed/UZvxWOiruss" frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen></iframe>
