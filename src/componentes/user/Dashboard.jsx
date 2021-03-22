3f60dae6f4d4d3c3985f04084137a650fcdca688
snow915 <cris_ganor@hotmail.com>
2021-03-22 17:03:21 -0600

    add: table of purchase history

--- src/componentes/user/Dashboard.jsx
+++ src/componentes/user/Dashboard.jsx
@@ -7,6 +7,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import DollarMarktComponent from "./DollarMarktComponent";
 import PaymentComponent from "./PaymentComponent";
 import {db} from "../config/firebase";
+import PurchaseHistory from "./PurchaseHistory";
 
 const Dashboard = () => {
     const {currentUser, logout} = useAuth();
@@ -47,28 +48,35 @@ const Dashboard = () => {
     const renderData = () => {
         if(logged){
             return (
-                <div className="row mt-5">
+                <div className="container-fluid">
+                    <div className="row mt-5">
 
-                    <div className="col-12 mt-5 d-flex justify-content-center">
-                        <img src={moneda_dashboard} className="img-fluid" style={{width: 400}} alt="MONEDA-SUNSHINE"/>
+                        <div className="col-12 mt-5 d-flex justify-content-center">
+                            <img src={moneda_dashboard} className="img-fluid" style={{width: 400}} alt="MONEDA-SUNSHINE"/>
+                        </div>
+
+                        <div className="col-12 d-flex justify-content-center">
+                            <h1>${userInfo.suns} <br/> Total SUN'S</h1>
+                        </div>
+
+                        <div className="col-12 d-flex justify-content-center mt-5">
+                            <button className="material" data-toggle="modal" data-target="#exampleModal">1 SUN = 00 00 00 00 USD <ExpandMoreIcon style={{ fontSize: 40 }} /> </button>
+                        </div>
+
+                        <div className="col-12">
+                            <button className="btn material2 ml-5 mr-5" data-toggle="modal" data-target="#paymentModal">Comprar</button>
+                        </div>
+
+                        <DollarMarktComponent />
+                        <PaymentComponent coinImage={moneda_dashboard} email={currentUser.email} userData={uid} />
+
                     </div>
 
-                    <div className="col-12 d-flex justify-content-center">
-                        <h1>${userInfo.suns} <br/> Total SUN'S</h1>
+                    <div className="row mt-5">
+                        <PurchaseHistory />
                     </div>
-
-                    <div className="col-12 d-flex justify-content-center mt-5">
-                        <button className="material" data-toggle="modal" data-target="#exampleModal">1 SUN = 00 00 00 00 USD <ExpandMoreIcon style={{ fontSize: 40 }} /> </button>
-                    </div>
-
-                    <div className="col-12">
-                        <button className="btn material2 ml-5 mr-5" data-toggle="modal" data-target="#paymentModal">Comprar</button>
-                    </div>
-
-                    <DollarMarktComponent />
-                    <PaymentComponent coinImage={moneda_dashboard} email={currentUser.email} userData={uid} />
-
                 </div>
+
             )
         } else {
             return null
