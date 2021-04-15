4eaada6db55bd4ca8542bc0a8f8f9ea8bd0c06f3
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-04-15 10:34:19 -0500

    se suben cambios en banner apra lazy loding que afecta carag diferida

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -3,11 +3,11 @@ import '../../App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.css';
 import monedag from "../../images/moneda.gif";
+import Banner from "./Banner";
 
 
 
-
-const Banner =lazy(()=>import ('./Banner'));
+/*const Banner =lazy(()=>import ('./Banner'));*/
 const Video =lazy(()=>import ('./Video'));
 const Countdown =lazy(()=>import ('./Countdown'));
 const Proyectos =lazy(()=>import ('../proyectos/Proyectos'));
@@ -25,14 +25,15 @@ const Home = () => {
 
     return (
         <div className="container-fluid" className="" >
+            <Banner/>
 <Suspense fallback={
 <div className="  container mt-5 text-center   justify-content-center">
 
-<img className="moneda-carga" loading="lazy" align="top"src={monedag}/><p className="">CARGANDO...</p> 
+<img className="moneda-carga" align="top"src={monedag}/><p className="">CARGANDO...</p> 
     
 </div>
 }>
-            <Banner/>
+            
             <Video/>
             <Countdown/>
             <WhitepaperHome />
