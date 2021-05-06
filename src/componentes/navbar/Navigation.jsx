e84e1bbb30f9ff88149aa0aed3d1253410799da3
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-05-06 16:05:06 -0500

    se modifica navbar

--- src/componentes/navbar/Navigation.jsx
+++ src/componentes/navbar/Navigation.jsx
@@ -60,12 +60,11 @@ const Navigation = () => {
         }
     };
 
-<<<<<<< HEAD
     const renderNavbar = () => {
         if(logged) {
 
             return (
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light" id="navbar">
                     <div className="container-fluid">
                         <NavLink className="navbar-brand navegacion  " to="/">
                             <img className="  " src={logonav}/>
@@ -73,42 +72,6 @@ const Navigation = () => {
                         <button className="navbar-toggler mr-auto" id="boton-nav" type="button" data-toggle="collapse" data-target="#navbar-menu"
                                 aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
-=======
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top  " id="navbar">
-            <div className="container-fluid ">
-                <NavLink className="navbar-brand navegacion  " to="/">
-                    <img className="  " src={logonav}/>
-                </NavLink>
-                <button className="navbar-toggler mr-auto" id="boton-nav" type="button" data-toggle="collapse" data-target="#navbar-menu"
-                        aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className=" collapse navbar-collapse" id="navbar-menu" >
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <NavLink className="navbar-brand nav-link navegacion" data-toggle="collapse" data-target=".navbar-collapse.show"
-                                     to="./Acerca">{t('Navbar.Acerca')}
-                            </NavLink>
-                        </li>
-                        <li className="nav-item ">
-
-                            <NavLink className="navbar-brand nav-link navegacion" tap-index="-1"
-                                     activeClassName="active" area-disabled="true" data-toggle="collapse" data-target=".navbar-collapse.show"
-                                     to="./Roaptomap">{t('Navbar.RoadToMap')}
-                            </NavLink>
-                        </li>
-                        <li className="nav-item ">
-
-                            <NavLink className="navbar-brand nav-link navegacion" activeClassName="active"
-                                     area-disabled="true" data-toggle="collapse" data-target=".navbar-collapse.show"
-                                     to="./Proyectos">{t('Navbar.Proyectos')}
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <a href="#" data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <button type="button" className="navsesion btn btn-link disabled" data-toggle="modal"
-                                 >{t('Navbar.SignIn')}
->>>>>>> master
                         </button>
                         <div className=" collapse navbar-collapse" id="navbar-menu" >
                             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
