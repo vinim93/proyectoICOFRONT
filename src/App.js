bb2c16f5b9692e543f97a4243d09210c354d85e4
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-05-12 11:31:25 -0500

    se corrrgige history anets merge con rama prueba

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'bootstrap/dist/css/bootstrap.css';
 
-import "./history.js";
+import "bootstrap/dist/css/manuel.js";
 
 import Navigation from "./componentes/navbar/Navigation";
 import {
