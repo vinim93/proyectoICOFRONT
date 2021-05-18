4ee6b0775d4b8b8bd86704fec054ea0ed620c7c7
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-05-18 12:29:10 -0500

    se  agregag traducccion beta  de walpaper

--- src/componentes/whitepaper/Pdf.jsx
+++ src/componentes/whitepaper/Pdf.jsx
@@ -3,11 +3,12 @@ import '../../App.css';
 import './Whitepaper.css';
 import Docpdf from '../../docs/Avance.pdf';
 import Loader from './Loader';
-
+import $ from "jquery";
 import {Document, Page} from 'react-pdf';
 import {pdfjs} from 'react-pdf';
 import {useTranslation} from 'react-i18next';
 import ControlPanel from './ControlPanel';
+import { StartValueType } from 'tsparticles/dist/Enums';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 
@@ -26,9 +27,19 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';*/
 
 
 const Pdf = ()=>{
+  
   const {t} = useTranslation();
-    
-    const [numPages, setNumPages] = useState(null);
+
+     const docupdf=[
+       {
+         pdfesp:t(`Whitepaper.Urlesp`)
+         
+      }
+     ];
+     
+
+     
+        const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
     const [isLoading, setIsLoading] =useState(true);
    const [scale,setScale]=useState(1.0);
@@ -36,15 +47,20 @@ const Pdf = ()=>{
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
         setIsLoading(false);
+        
       }
    
             return(
              
-           <div className="row   " >
-            
-            <div className="col-12  fondopdf p-3 d-none d-md-block" >
+           <div className="row" >
+              
+             {
+            docupdf.map((value,index)=>(
+            <div id={index} className="col-12  fondopdf p-3 d-none d-md-block" >
            
-            < iframe width='800' data-toggle="collapse" data-target=".navbar-collapse.show" className="google-viewer" height='6https00' frameborder='0' src="https://firebasestorage.googleapis.com/v0/b/sunshine-ico.appspot.com/o/general%2FWHITEPAPER%20TRON%20(2)%20activos%20digitales.pdf?alt=media&token=3c94b954-b6d3-47de-9233-a01c2e06849c"></iframe>
+            < iframe width='800' data-toggle="collapse" data-target=".navbar-collapse.show" className="google-viewer" height='600' 
+            frameborder='0' src={value.pdfesp} ></iframe>
+
             <Loader isLoading={isLoading}/>
                  
      
@@ -52,7 +68,11 @@ const Pdf = ()=>{
               
              
                 </div>
-                <div className="col-12 fondopdf p-3 d-block d-md-none" >
+                ))
+                }
+                 {
+            docupdf.map((value,index)=>(
+                <div id={index}className="col-12 fondopdf p-3 d-block d-md-none" >
                <Loader isLoading={isLoading}/>
                <ControlPanel className=""
                  numPages={numPages}                 
@@ -60,13 +80,18 @@ const Pdf = ()=>{
                  setPageNumber={setPageNumber} 
                  setScale={setScale}/>
               
-              <Document className=" pdfcontainer "
-        file= {t("Whitepaper.urLEsp")}
+              <Document className="pdfcontainer " id={index}
+        file= {value.pdfesp}
+          
+        
+          
         onLoadSuccess={onDocumentLoadSuccess}
       >
         <Page pageNumber={pageNumber} className="   "scale={scale} />
       </Document>
                 </div>
+                 ))
+                }
                 </div>
             
         
