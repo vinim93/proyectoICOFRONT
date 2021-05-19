dcd44332c8031d2c7f33fc96f72450b4c055c24c
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-05-19 11:24:49 -0500

    se arregla problema con cors   de pdf-react

--- src/componentes/whitepaper/Pdf.jsx
+++ src/componentes/whitepaper/Pdf.jsx
@@ -1,105 +1,91 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import './Whitepaper.css';
 import Docpdf from '../../docs/Avance.pdf';
 import Loader from './Loader';
 import $ from "jquery";
-import {Document, Page} from 'react-pdf';
-import {pdfjs} from 'react-pdf';
-import {useTranslation} from 'react-i18next';
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import { useTranslation } from 'react-i18next';
 import ControlPanel from './ControlPanel';
 import { StartValueType } from 'tsparticles/dist/Enums';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-
-
-
-
-
 
 /*import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';*/
 
 
+const Pdf = () => {
 
+  const { t } = useTranslation();
 
+  const docupdf = [
+    {
+      pdfesp: t(`Whitepaper.Urlesp`),
+      pdfespG:t(`Whitepaper.UrlespG`)
 
-
-const Pdf = ()=>{
-  
-  const {t} = useTranslation();
-
-     const docupdf=[
-       {
-         pdfesp:t(`Whitepaper.Urlesp`)
-         
-      }
-     ];
-     
-
-     
-        const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [isLoading, setIsLoading] =useState(true);
-   const [scale,setScale]=useState(1.0);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setIsLoading(false);
-        
-      }
-   
-            return(
-             
-           <div className="row" >
-              
-             {
-            docupdf.map((value,index)=>(
-            <div id={index} className="col-12  fondopdf p-3 d-none d-md-block" >
-           
-            < iframe width='800' data-toggle="collapse" data-target=".navbar-collapse.show" className="google-viewer" height='600' 
-            frameborder='0' src={value.pdfesp} ></iframe>
-
-            <Loader isLoading={isLoading}/>
-                 
-     
-              
-              
-             
-                </div>
-                ))
-                }
-                 {
-            docupdf.map((value,index)=>(
-                <div id={index}className="col-12 fondopdf p-3 d-block d-md-none" >
-               <Loader isLoading={isLoading}/>
-               <ControlPanel className=""
-                 numPages={numPages}                 
-                 pageNumber={pageNumber}
-                 setPageNumber={setPageNumber} 
-                 setScale={setScale}/>
-              
-              <Document className="pdfcontainer " id={index}
-        file= {value.pdfesp}
-          
-        
-          
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} className="   "scale={scale} />
-      </Document>
-                </div>
-                 ))
-                }
-                </div>
-            
-        
-            
-        );
     }
+  ];
 
 
 
-        export default Pdf;
-        
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [scale, setScale] = useState(1.0);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setIsLoading(false);
+
+  }
+
+  return (
+
+    <div className="row" >
+
+      {
+        docupdf.map((value, index) => (
+          <div id={index} className="col-12  fondopdf p-3 d-none d-md-block" >
+
+            < iframe width='800' data-toggle="collapse" data-target=".navbar-collapse.show" className="google-viewer" height='600' crossorigin="anonymous"
+              frameborder='0' src={value.pdfespG} ></iframe>
+
+            <Loader isLoading={isLoading} />
+          </div>
+        ))
+      }
+
+      {
+        docupdf.map((value, index) => (
+          <div id={index} className="col-12 fondopdf p-3 d-block d-md-none">
+            <Loader isLoading={isLoading} />
+            <ControlPanel className=""
+              numPages={numPages}
+              pageNumber={pageNumber}
+              setPageNumber={setPageNumber}
+              setScale={setScale} />
+            {console.log(value.pdfesp)}
+            <Document className="pdfcontainer " id={index} 
+            
+              file={value.pdfesp}
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+
+              <Page pageNumber={pageNumber} className="   " scale={scale} />
+
+            </Document>
+          </div>
+        ))
+      }
+    </div>
+
+
+
+  );
+}
+
+
+
+export default Pdf;
