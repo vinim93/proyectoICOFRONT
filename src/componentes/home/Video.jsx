5b9815788010792b2618d4197e91dc127f2370dc
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-06-14 11:51:13 -0500

    se realiza cambios en animaciones

--- src/componentes/home/Video.jsx
+++ src/componentes/home/Video.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import Flip from 'react-reveal/Flip';
+
 
 const Video = () => {
     return (
         <div className="row   ">
-  <Flip right>
+
             <div className="col-12 videosunrep mt-5 p-0">
           
                 <iframe src="https://www.youtube.com/embed/O5c2yMxyL54" frameBorder="0" 
@@ -13,7 +13,7 @@ const Video = () => {
                         allowFullScreen></iframe>
                        
             </div>
-            </Flip>
+            
 
         </div>
     );
