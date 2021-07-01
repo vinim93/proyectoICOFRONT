826ae5d86efef2062cfa192c76d2d24186b5b79e
sunshinejv <jose.viniegra@sunshine-imagine.com>
2021-07-01 13:42:17 -0500

    se cambia url de  video sunshine

--- src/componentes/home/Video.jsx
+++ src/componentes/home/Video.jsx
@@ -8,7 +8,7 @@ const Video = () => {
 
             <div className="col-12 videosunrep mt-5 p-0">
           
-                <iframe src="https://www.youtube.com/embed/9_2Rdr6ADEI" title="YouTube video player" frameBorder="0" 
+                <iframe src="https://www.youtube.com/embed/2eHFAx6K9Dg" title="YouTube video player" frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen></iframe>
                        
